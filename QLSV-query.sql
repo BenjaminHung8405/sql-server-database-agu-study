a7ff867e3408b7793ae18933e0d27955352a4d9d
BenjaminHung8405 <nphung8405@gmail.com>
2024-10-29 20:25:25 +0700

    First step of SQL

---
+++ QLSV-query.sql
@@ -0,0 +1,11 @@
+create database QLSV
+on
+(
+	name = 'QLSV_dat',
+	filename = 'D:\Study\AGU\CSDL\SQL-Project\QLSV_dat.mdf'
+)
+log on
+(
+	name = 'QLSV_log',
+	filename = 'D:\Study\AGU\CSDL\SQL-Project\QLSV_log.ldf'
+)
